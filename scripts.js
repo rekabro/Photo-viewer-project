d6ca7a6543418767c3c329f2d413bf3945101157
rnegyzet <63166314+rnegyzet@users.noreply.github.com>
2020-04-22 18:41:57 +0200

    Finalizing descritions

--- scripts.js
+++ scripts.js
@@ -16,24 +16,29 @@ let data = {
   }
   /*loadPhoto(currentPhoto);*/
 
-  let titles = ['Útnak indulunk', 'Az ifjú pár', 'Önfeledt falukázás Varságon', 'A világ tetején', 'Ha az Úr velünk...', 'Szerelem a Madarason', 'Egymásba feledkezve', 'Kezdődik az utunk'];
+  let titles = ['Útnak indulunk', 'Az ifjú pár', 'Önfeledt falukázás Varságon', 'A világ tetején', '"Ha az Úr velünk...', 'Szerelem a Madarason', 'Egymásba feledkezve', 'Kezdődik az utunk'];
   let ezaTitle = 0;
   let loadTitle = (titleNumber) => {
   $('#title').append(titles[titleNumber]);
   }
   
   let descriptions = ['"Biztosan eljön az Úr, mint a hajnal, eljön hozzánk, mint az őszi eső, és mint a tavaszi eső a földre" /Oz 6,3/',
-                      '"Ahogy engem szeretett az Atya, úgy szerettelek én is titeket. Maradjatok meg szeretetemben... Azért mondtam nektek ezeket, hogy az én örömöm bennetek legyen, és örömötök teljes legyen. /Jn 15, 9-11/"',
-                      'A pillanat, amit ki kellene tágítani, hogy örökké tartson - hogy ez a végtelen érzés a mélyben megmaradjon',
-                      'Egy egység vagyunk most, két út, mi végül összért',
-                      '.. ki lehet ellenünk?!',
-                      '"Nézzétek az ég madarait, nézzétek a rét virágait: csűrökbe sosem gyűjtenek. Miért nyugtalan a szívetek?"',
-                      'szerelem, szenvedély',
-                      ' Megvárni egy angyal türelmével...']
+                      '"Ahogy engem szeretett az Atya, úgy szerettelek én is titeket. Maradjatok meg szeretetemben... Azért mondtam nektek ezeket, hogy az én örömöm bennetek legyen, és örömötök teljes legyen." <br>/Jn 15, 9-11/',
+                      '"A reggel tüzes tőrként pislog szemembe, szikrák párnáján alszom öledben csöndbe ölelve,<br> deres erek díszében dobog a hajnal ébredése életre hív napsugárhúrjain a szerelem szívverése." <br> /BV: Szikrapárnán/',
+                      '"De egyszer csak találkozunk… A télben pillantásod megölel, megszűnik tér, idő – távol vagy tőlem és mégis közel - átizzik közöttünk az éter, felkérlek egy táncra - tűzben forgunk, az otthon melege ég retinámba." /BV: Testvértekintet/',
+                      '.. ki lehet ellenünk? Ő, aki tulajdon Fiát sem kímélte, hanem odaadta értünk, mindnyájunkért, ne ajándékozna vele együtt mindent nekünk?" /Róm 8,31-32/',
+                      '"Nézzétek az ég madarait: nem vetnek, nem aratnak, csűrökbe sem gyűjtenek, és a ti mennyei Atyátok táplálja őket. Nem értek ti sokkal többet ezeknél?" /Mt 6,26/',
+                      'Két szív iker-dobbanása csöndünkbe ölelve egy ütemre vérez <br> láng-bőrödbe öltözött arcom meztelen-vakon vezetve érint és érez" <br>/BV: Csókvarázs/',
+                      '"Megvárni, egy angyal és egy szent türelmével, amíg a dolgok - emberek, eszmék, helyzetek -, melyek hozzád tartoznak, eljutnak hozzád.. Ha nagyon sietsz feléjük, elkerülheted azt, ami fontos és személyesen a tiéd. Várj, nagy erővel, figyelmesen, egész sorsoddal és életeddel." /M. S./']
   let ezaLeiras = 0;
   let loadLeiras = (leirasNumber) => {
     $('#description').append(descriptions[leirasNumber]);
     }
+let oneScreen = {
+  kep: kepek[0],
+  cim: titles[0],
+  leiras: descriptions [0],
+};
 
   $('#rightArrow').click(() => {
  if (currentPhoto<7) { 
